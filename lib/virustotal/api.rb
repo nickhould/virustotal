385391b7b88593e9cecf7f590408035c04a915fe
Jean-Nicholas Hould <nick.hould@gmail.com>
2013-09-22 19:59:57 -0400

    Add JSON decoding on API responses

--- lib/virustotal/api.rb
+++ lib/virustotal/api.rb
@@ -1,13 +1,5 @@
 module Virustotal
   module API
-
-    def self.base_uri
-      "https://www.virustotal.com/vtapi/v2"
-    end
-
-    def self.api_key
-      # Set your API key here
-    end
-
+    ENDPOINT = "https://www.virustotal.com/vtapi/v2"
   end
 end
--- lib/virustotal/client.rb
+++ lib/virustotal/client.rb
@@ -1,68 +1,102 @@
 require "open-uri"
+require "json"
+
 module Virustotal
   class Client
 
+    attr_accessor :api_key, :user_agent
+
+    def initialize(options={})
+      for key, value in options
+        send(:"#{key}=", value)
+      end
+      yield self if block_given?
+      validate_credentials_type!
+    end
+
+    # Configuration
+    def self.api_key
+      instance_variable_defined?(:@api_key) ? @api_key : ENV["api_key"]
+    end
+
+    def self.user_agent
+      @user_agent ||= "VirusTotal Ruby Gem #{Virustotal::VERSION}"
+    end
+
+    # Requests
     # Sending and scanning files
-    def self.file_scan(file_path_or_uri)
-      options.merge!({ file: open( file_path_or_uri ) })
-      RestClient.post endpoint("/file/scan"), options
+    def file_scan(file_path_or_uri)
+      merged_options = options.merge({ file: open( file_path_or_uri ) })
+      response = RestClient.post endpoint("/file/scan"), merged_options
+      JSON.parse response
     end
 
     # Rescanning already submitted files
-    def self.file_rescan(resources)
+    def file_rescan(resources)
       resources = format(resources)
-      options.merge!({ resource: resources})
-      RestClient.post endpoint("/file/rescan"), options
+      merged_options = options.merge({ resource: resources})
+      response = RestClient.post endpoint("/file/rescan"), merged_options
+      JSON.parse response
     end
 
     # Retrieving file scan reports
-    def self.file_report(resources)
+    def file_report(resources)
       resources = format(resources)
-      options.merge!({ resource: resources })
-      RestClient.post endpoint("/file/report"), options
+      merged_options = options.merge({ resource: resources })
+      response = RestClient.post endpoint("/file/report"), merged_options
+      JSON.parse response
     end
 
     # Sending and scanning URLs
-    def self.url_scan(url)
-      options.merge!({ url: url })
-      RestClient.post endpoint("/url/scan"), options
+    def url_scan(url)
+      merged_options = options.merge({ url: url })
+      response = RestClient.post endpoint("/url/scan"), merged_options
+      JSON.parse response
     end
 
     # Retrieving URL scan reports
-    def self.url_report(resource) # resource: scan_id or url
-      options.merge!({ resource: resource })
-      RestClient.post endpoint("/url/report"), options
+    def url_report(resource) # resource: scan_id or url
+      merged_options = options.merge!({ resource: resource })
+      response = RestClient.post endpoint("/url/report"), merged_options
+      JSON.parse response
     end
 
     # Retrieving IP address reports
-    def self.ip_adress_report(ip_adress)
-      options.merge!({ ip: ip_adress })
-      RestClient.get endpoint("/ip-address/report"), params: options
+    def ip_adress_report(ip_adress)
+      merged_options = options.merge({ ip: ip_adress })
+      response = RestClient.get endpoint("/ip-address/report"), params: merged_options
+      JSON.parse response
     end
 
     # Retrieving domain reports
-    def self.domain_report(domain)
-      options.merge!({ domain: domain})
-      RestClient.get endpoint("/domain/report"), params: options
+    def domain_report(domain)
+      merged_options = options.merge({ domain: domain})
+      response = RestClient.get endpoint("/domain/report"), params: merged_options
+      JSON.parse response
     end
 
     # Make comments on files and URLs
-    def self.comments_put(resource, comment)
-      options.merge!({ resource: resource, comment: comment })
-      RestClient.post endpoint("/comments/put"), options
+    def comments_put(resource, comment)
+      merged_options = options.merge({ resource: resource, comment: comment })
+      response = RestClient.post endpoint("/comments/put"), merged_options
+      JSON.parse response
     end
 
     private
-    def self.options
-      @options ||= { apikey: Virustotal::API.api_key }
+    def options
+      @options ||= { apikey: api_key, user_agent: user_agent }
     end
 
-    def self.endpoint(path)
-      Virustotal::API.base_uri + path
+    def endpoint(path)
+      Virustotal::API::ENDPOINT + path
     end
 
     def format(resources)
       resources.class == Array ? resources.join(", ") : resources
+    end
+
+    def validate_credentials_type!
+      raise(InvalidCredentials, "Invalid API key. Must be a string.") if !api_key.nil? && !api_key.is_a?(String)
     end
   end
 end
